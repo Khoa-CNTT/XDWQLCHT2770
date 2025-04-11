081491dd4ce5eabad4a00461d2bbdbb759f1b283
vietducoday <vietducdtu@gmail.com>
2025-04-11 21:38:06 +0700

    sua userjs

--- frontend/src/routers/user.js
+++ frontend/src/routers/user.js
@@ -15,20 +15,49 @@ export default [
       },
       {
         path: "post",
-        name: "Post" ,
+        name: "Post",
         component: () => import("../pages/PageUser/Post.vue"),
       },
       {
         path: "room",
-        name: "Room" ,
+        name: "Room",
         component: () => import("../pages/pageUser/Rooms.vue"),
       },
       {
         path: "roomdetail",
-        name: "RoomDetail" ,
+        name: "RoomDetail",
         component: () => import("../pages/pageUser/RoomDetails.vue"),
       },
-
+      {
+        path: "login",
+        name: "Login",
+        component: () => import("../pages/pageUser/Login.vue"),
+      },
+      {
+        path: "register",
+        name: "Register",
+        component: () => import("../pages/pageUser/Register.vue"),
+      },
+      {
+        path: "postdetail",
+        name: "PostDetail",
+        component: () => import("../pages/pageUser/PostDetails.vue"), 
+      },
+      {
+        path: "login",
+        name: "Login",
+        component: () => import("../pages/pageUser/Login.vue"),
+      },
+      {
+        path: "register",
+        name: "Register",
+        component: () => import("../pages/pageUser/Register.vue"),
+      },
+      {
+        path: "postdetail",
+        name: "PostDetail",
+        component: () => import("../pages/pageUser/PostDetails.vue"), 
+      },
     ],
   },
 ];
